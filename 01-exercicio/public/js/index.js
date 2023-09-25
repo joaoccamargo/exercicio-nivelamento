af879f90037a7a91ffb7a15976368ab5a6e66536
João Camargo <jclaudio00_12@hotmail.com>
2023-09-25 15:25:34 -0300

    01-exercicio finalizado

--- 01-exercicio/public/js/index.js
+++ 01-exercicio/public/js/index.js
@@ -1,6 +1,16 @@
-var largura_terreno = document.getElementById("largura_terreno")
-document.getElementById("comprimento_terreno");
-document.getElementById("valor_metroq");
+var largura_terreno = document.getElementById("largura_terreno");
+var comprimento_terreno = document.getElementById("comprimento_terreno");
+var valor_metro_quadrado = document.getElementById("valor_metroq");
+var resultado_calculo = document.getElementById("resultado_calculo");
 
 
-largura_terreno.value = "Jose"
+function calcularArea(largura, comprimento){
+    largura = largura_terreno.value;
+    comprimento = comprimento_terreno.value;
+    let area = (comprimento * largura);
+    let preco = (valor_metro_quadrado.value * area);
+
+
+    resultado_calculo.innerText = `Área do terreno = ${area.toFixed(2)} \n 
+                                   Preço do terreno = ${preco.toFixed(2)}`;
+}
