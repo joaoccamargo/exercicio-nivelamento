3c34a622b100acfd35beb16335fab95586946541
João Camargo <jclaudio00_12@hotmail.com>
2023-09-25 14:58:31 -0300

    initial commit

---
+++ 01-exercicio/public/js/index.js
@@ -0,0 +1,6 @@
+var largura_terreno = document.getElementById("largura_terreno")
+document.getElementById("comprimento_terreno");
+document.getElementById("valor_metroq");
+
+
+largura_terreno.value = "Jose"
