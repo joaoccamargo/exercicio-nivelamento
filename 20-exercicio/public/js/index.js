0db95c6f454b399da7f1b0c2529343d69cf28c23
João Camargo <jclaudio00_12@hotmail.com>
2023-09-26 13:52:26 -0300

    20-exercicio

---
+++ 20-exercicio/public/js/index.js
@@ -0,0 +1,17 @@
+let numero_inicial = document.getElementById('numero_inicial')
+let numero_final = document.getElementById('numero_final');
+let resultado = document.getElementById('resultado')
+let soma = 0;
+
+let somarNumeros = () => {
+  let inicial = parseInt(numero_inicial.value);
+  let final = parseInt(numero_final.value);
+
+  if(inicial <= final){
+    for (let i = inicial; i <= final; i++) {
+      soma += i;
+    }
+  }
+
+  resultado.innerHTML = `Resultado: ${soma}`
+}
